f75c5defaae47051cc14131af0890642c5fa1aad
Vishal Kamble <kamblevishal.5189@gmail.com>
2021-08-26 22:33:36 +0100

    my commit

--- src/components/plotlyDash.js
+++ src/components/plotlyDash.js
@@ -6,7 +6,7 @@ function PlotlyDashboard(){
     return(
         <div>
 
-        <h1>Hello World</h1>
+        <h1>Hello Worlds</h1>
            <iframe src='https://dash-gallery.plotly.host/ddk-clinical-trial-dashboard/'/>
 
             
